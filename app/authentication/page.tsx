3ff8a16aded01781f7f6e2dea7e45dfe8678b32c
syedAwadh <mohammed.awadh@gyanmatrix.com>
2023-09-16 18:08:08 +0530

    initial routes

---
+++ app/authentication/page.tsx
@@ -0,0 +1,9 @@
+import AuthenticationComponent from "@/components/Organelles/Authentication"
+
+export default function AuthenticationLoginPage () {
+    return (
+        <>
+            <AuthenticationComponent/>
+        </>
+    )
+}
